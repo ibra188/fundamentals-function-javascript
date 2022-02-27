ce39c056df001c3be0002006ad4112f0097ed49d
EBRIMA SONKO <82295266+ibra188@users.noreply.github.com>
2022-02-27 01:11:56 +0100

    keep the strok with js at frontendmaester

--- script.js
+++ script.js
@@ -1,84 +1,56 @@
 
-const cars = [
-    "Saab",
-    "Lancia",
-    "BMW"
-];
-document.getElementById("demo").innerHTML = cars;
-
-const car = [
-    "Saab",
-    "Lancia",
-    "BMW"
-];
-document.getElementById("demo-2").innerHTML =car[0];
-
-const fruitsOne = [
-    "Banana",
-    "Orange",
-    "Apple"];
-document.getElementById("demo-9").innerHTML =
-    fruitsOne instanceof Array;
-
-const fruits = [
-    "Banana",
-    "Oranges",
-    "Apple"
-];
-document.getElementById("demo-3").innerHTML = fruits;
-fruits[0] =  "Kiwi";
-document.getElementById("demo-4").innerHTML = fruits;
-
-const fruit = [
-    "Banana",
-    "Oranges",
-    "Apple",
-    "Mango"
-];
-document.getElementById("demo-5").innerHTML =
-    fruit.length;
-
-const myFruits = [
-    "Banana",
-    "Oranges",
-    "Mango",
-    "Apple"
-];
-let myFruitsLength = myFruits.length;
-let text = "<ul>";
-for(let i = 0; i <myFruitsLength; i++ ){
-    text += "<li>" + myFruits[i] + "</li>";
+const num = 3;
+function multiplyBy2(inputNumber){
+    const result = inputNumber*2;
+    return result;
 }
-text += "</ul>";
-document.getElementById("demo-6").innerHTML = text;
-const verdura = [
-    "Banana",
-    "Apple",
-    "Mango",
-    "kiwi"
-];
-document.getElementById("demo-7").innerHTML =
-    verdura;
 
-function myFunction(){
-    verdura[verdura.length] = "lemon";
-    document.getElementById("demo-7").innerHTML =verdura;
+const out = multiplyBy2(num);
+const newoutput = multiplyBy2(10);
+
+console.log('out')
+
+function tenSquared(){
+    return 10*10;
 }
-const verdura2 = [
-    "Banana",
-    "Apple",
-    "Mango",
-    "Kiwi"
-];
-verdura2[6] = "lemon";
-let vlength = verdura2.length;
-let text2 = "";
-for(let i = 0; i < vlength; i++ ){
-    text2 += vlength[i] + "<br>";
+
+tenSquared()//100
+
+//Drey principles
+function squareNum(num){
+    return num*num;
 }
-document.getElementById("demo-8").innerHTML =
-    text2;
+squareNum(10);//100
+squareNum(9)//81
+squareNum(8)//64
 
+function copyArrayAndMultiplyBy2(array){
+    const output =[];
+    for(let i = 0; i < array.length; i++){
+        output.push(array[i] * 2);
+    }
+    return output;
+}
 
-const fruits5 = ["Banana", "Orange", "Apple"];
-document.getElementById("demo-9").innerHTML = fruits5 instanceof Array;
+const myArray = [1,2,3];
+const result = copyArrayMultipleBy2(myArray);
+
+//Array function
+function multiplyBy2(input){return input * 2;}
+
+const multiplyBy2 = (input) => {return input*2}
+
+const multiplyBy2 = (input) => input*2
+
+const copyArrayMultipleBy2 = input => input*2
+
+//closure
+ 
+function creteFunction(){
+    function multiplyBy2(num){
+        return num*2;
+    }
+    return multiplyBy2;
+}
+const generatedFunc = createFunction();
+const result = generatedFunc(3);
